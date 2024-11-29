df8c3f04754d8f1c80f2af4af60860f5d110e604
Vicentesan <vikom.sanchez@gmail.com>
2024-11-29 15:27:51 -0300

    chore

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -9,64 +9,62 @@ import { groups, member, users } from './schemas'
 
 export const db = drizzle(env.DATABASE_URL, { schema, logger: true })
 
-async function seedDatabase() {
-  await db.transaction(async (tx) => {
-    await reset(tx, schema)
+// async function seedDatabase() {
+//   await db.transaction(async (tx) => {
+//     await reset(tx, schema)
 
-    const owner = await tx
-      .insert(users)
-      .values({
-        name: 'John Doe',
-        email: 'john.doe@example.com',
-        passwordHash: await hash('12345678', 1),
-        avatarUrl: faker.image.avatarGitHub(),
-        plan: 'BASIC',
-      })
-      .returning()
+//     const owner = await tx
+//       .insert(users)
+//       .values({
+//         name: 'John Doe',
+//         email: 'john.doe@example.com',
+//         passwordHash: await hash('12345678', 1),
+//         avatarUrl: faker.image.avatarGitHub(),
+//         plan: 'BASIC',
+//       })
+//       .returning()
 
-    const group = await tx
-      .insert(groups)
-      .values({
-        name: faker.company.name(),
-        description: faker.lorem.sentence(),
-        budget: '300',
-        avatarUrl: faker.image.avatarGitHub(),
-        timesMatchesGenerated: 0,
-        ownerId: owner[0].id,
-        endDate: new Date(faker.date.future()),
-        drawDate: new Date(faker.date.future()),
-      })
-      .returning()
+//     const group = await tx
+//       .insert(groups)
+//       .values({
+//         name: faker.company.name(),
+//         description: faker.lorem.sentence(),
+//         budget: '300',
+//         avatarUrl: faker.image.avatarGitHub(),
+//         timesMatchesGenerated: 0,
+//         ownerId: owner[0].id,
+//         endDate: new Date(faker.date.future()),
+//         drawDate: new Date(faker.date.future()),
+//       })
+//       .returning()
 
-    await tx.insert(member).values({
-      groupId: group[0].id,
-      userId: owner[0].id,
-      role: 'ADMIN',
-    })
+//     await tx.insert(member).values({
+//       groupId: group[0].id,
+//       userId: owner[0].id,
+//       role: 'ADMIN',
+//     })
 
-    const members = []
+//     const members = []
 
-    for (let i = 0; i < 10; i++) {
-      const user = await tx
-        .insert(users)
-        .values({
-          name: faker.person.fullName(),
-          email: faker.internet.email().toLowerCase(),
-          passwordHash: faker.internet.password(),
-          avatarUrl: faker.image.avatarGitHub(),
-          plan: faker.helpers.arrayElement(['BASIC', 'PRO']),
-        })
-        .returning()
+//     for (let i = 0; i < 10; i++) {
+//       const user = await tx
+//         .insert(users)
+//         .values({
+//           name: faker.person.fullName(),
+//           email: faker.internet.email().toLowerCase(),
+//           passwordHash: faker.internet.password(),
+//           avatarUrl: faker.image.avatarGitHub(),
+//           plan: faker.helpers.arrayElement(['BASIC', 'PRO']),
+//         })
+//         .returning()
 
-      await tx.insert(member).values({
-        groupId: group[0].id,
-        userId: user[0].id,
-        role: 'MEMBER',
-      })
+//       await tx.insert(member).values({
+//         groupId: group[0].id,
+//         userId: user[0].id,
+//         role: 'MEMBER',
+//       })
 
-      members.push(user[0].id)
-    }
-  })
-}
-
-if (env.NODE_ENV === 'development') seedDatabase()
+//       members.push(user[0].id)
+//     }
+//   })
+// }
--- apps/api/src/env/index.ts
+++ apps/api/src/env/index.ts
@@ -9,7 +9,6 @@ const envSchema = z.object({
   GOOGLE_REDIRECT_URI: z.string().url(),
   NEXT_PUBLIC_BASE_URL: z.string().url(),
   HOST: z.string().default('0.0.0.0'),
-  NODE_ENV: z.enum(['development', 'production']).default('development'),
 })
 
 export const env = envSchema.parse(process.env)
