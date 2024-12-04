acc17b463f78200ca3f90fb4d35928e8154f805b
Vicentesan <vikom.sanchez@gmail.com>
2024-12-04 11:15:19 -0300

    feat(deploy): add base api deploy dockerfile structure

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src'],
   outDir: 'dist',
+  format: ['esm'],
   splitting: false,
-  sourcemap: true,
   clean: true,
   noExternal: ['@migos/auth'],
 })
